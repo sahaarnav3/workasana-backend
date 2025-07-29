7143dcd6623741fd8c7ce5ce493c798d8f8604f8
sahaarnav3 <sahaarnav3@gmail.com>
2025-07-29 18:19:57 +0530

    Added route to post and get projects.

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const verifyJWT = (req, res, next) => {
 
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    console.log(decodedToken);
+    // console.log(decodedToken);
     req.user = decodedToken;
     next();
   } catch (error) {
@@ -138,6 +138,67 @@ app.post("/auth/login", async (req, res) => {
       { expiresIn: "7 days" }
     );
     return res.status(200).json({ userToken: token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error:
+        "Some error occurred with the request itself. Please check logs and try again.",
+    });
+  }
+});
+
+//Route to fetch current logged in user details, based on token.
+app.get("/auth/me", verifyJWT, async (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error:
+        "Some error occurred with the request itself. Please check logs and try again.",
+    });
+  }
+});
+
+//----------------------------------------------------------------------//
+
+app.post("/projects", verifyJWT, async (req, res) => {
+  try {
+    if (!(req.body.name.length > 0 && req.body.description.length > 0))
+      return res
+        .status(400)
+        .json({ error: "Please enter a proper name and description." });
+
+    const project = new Project(req.body);
+    const projectResponse = await project.save();
+    if (!projectResponse)
+      return res.status(400).json({
+        error:
+          "Some error occurred. The request couldn't be saved. Please try again.",
+      });
+    return res.status(200).json({ message: "Project Added Successfully." });
+  } catch (error) {
+    if (error.code == 11000)
+      return res
+        .status(400)
+        .json({
+          error:
+            "Project with entered Name already Exists. Please try again with different Project Name.",
+        });
+    console.log(error);
+    res.status(500).json({
+      error:
+        "Some error occurred with the request itself. Please check logs and try again.",
+    });
+  }
+});
+
+app.get("/projects", verifyJWT, async (req, res) => {
+  try {
+    const projectResponse = await Project.find();
+    if(!projectResponse)
+      return res.status(400).json({ error: "No Projects Present. Please enter some projects first." })
+    return res.status(200).json(projectResponse)
   } catch (error) {
     console.log(error);
     res.status(500).json({
