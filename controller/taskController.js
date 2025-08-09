93c0487d46f39ed9789051d42a2d2e08bb8234b0
sahaarnav3 <sahaarnav3@gmail.com>
2025-08-09 23:38:24 +0530

    Added routes to update and delete tasks.

--- controller/taskController.js
+++ controller/taskController.js
@@ -39,25 +39,65 @@ module.exports.createNewTask = async (req, res) => {
 module.exports.fetchTasks = async (req, res) => {
   try {
     const searchQuery = {};
-    if(req.query.project)
-        searchQuery.project = req.query.project;
-    if(req.query.team)
-        searchQuery.team = req.query.team;
-    if(req.query.owner && req.query.owner.split(" ").length > 0)
-        searchQuery.owners = req.query.owner.split(" ");
-    if(req.query.tags && req.query.tags.split(" ").length > 0)
-        searchQuery.tags = req.query.tags.split(" ");
-    if(req.query.timeToComplete)
-        searchQuery.timeToComplete = req.query.timeToComplete;
-    if(req.query.status)
-        searchQuery.status = req.query.status;
+    if (req.query.project) searchQuery.project = req.query.project;
+    if (req.query.team) searchQuery.team = req.query.team;
+    if (req.query.owner && req.query.owner.split(" ").length > 0)
+      searchQuery.owners = req.query.owner.split(" ");
+    if (req.query.tags && req.query.tags.split(" ").length > 0)
+      searchQuery.tags = req.query.tags.split(" ");
+    if (req.query.timeToComplete)
+      searchQuery.timeToComplete = req.query.timeToComplete;
+    if (req.query.status) searchQuery.status = req.query.status;
     // console.log(searchQuery);
     const taskResponse = await Task.find(searchQuery)
       .populate(["project", "team", "owners"])
       .exec();
-      if(!taskResponse || taskResponse.length < 1)
-        return res.status(400).json({ error: "No task with given filter value(s) found. Please try again." });
+    if (!taskResponse || taskResponse.length < 1)
+      return res.status(400).json({
+        error: "No task with given filter value(s) found. Please try again.",
+      });
     return res.status(200).json(taskResponse);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error:
+        "Some error occurred with the request itself. Please check logs and try again.",
+    });
+  }
+};
+
+//Controller to update a task.
+module.exports.updateTask = async (req, res) => {
+  try {
+    const taskResponse = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+      .populate(["project", "team", "owners"])
+      .exec();
+    if (!taskResponse)
+      return res
+        .status(404)
+        .json({
+          error:
+            "Task with given ID not found. Please try again with correct ID.",
+        });
+    return res.status(200).json(taskResponse);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error:
+        "Some error occurred with the request itself. Please check logs and try again.",
+    });
+  }
+};
+
+//Controller to delete a task.
+module.exports.deleteTask = async (req, res) => {
+  try {
+    const taskResponse = await Task.findByIdAndDelete(req.params.id);
+    if(!taskResponse)
+      return res.status(404).json({ error: "Task with given ID doesn't exist. Try again with correct ID."})
+    return res.status(200).json({ message: "Task deleted successfully."});
   } catch (error) {
     console.log(error);
     res.status(500).json({
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -11,4 +11,10 @@ router.post("/", verifyJWT, taskController.createNewTask);
 //Route to fetch tasks with or without filters
 router.get("/", verifyJWT, taskController.fetchTasks);
 
+//Route to update tasks with id
+router.post("/:id", verifyJWT, taskController.updateTask);
+
+//Route to delete task with id.
+router.delete("/:id", verifyJWT, taskController.deleteTask);
+
 module.exports = router;
